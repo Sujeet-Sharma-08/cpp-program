bd1f8fbb07561c86ecaa4e11e261b5a63f38c068
Sujeet Kumar Sharma <125545129+Sujeet-Sharma-08@users.noreply.github.com>
2023-06-06 11:08:31 +0530

    Add files via upload

---
+++ fibbonacci series by recursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+	if(n==0){
+		return 0;
+	}
+	if(n==1){
+		return 1;
+	}
+	int res = fib(n-1)+fib(n-2);
+	return res;
+}
+int main(){
+	
+	cout<<fib(6);
+	return 0;
+}
